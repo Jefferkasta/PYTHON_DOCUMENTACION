e3687f72e0ad8ddb4d385ec12015c2762f90c20a
Jefferson <jeferkasta@hotmail.com>
2023-10-04 22:05:14 -0500

    Adicione archivos nuevos

--- 14-diccionarios.py
+++ 14-diccionarios.py
@@ -1,2 +1,2 @@
 productos = {'Arroz':'2500','Lentejas':'2700','Frijo':'3500','Espaguette':'1800','Atun':'4700'}
-print ("El varlor del producto es: ",productos["Arroz"])
+print (type("El varlor del producto es: ",productos["Arroz"]))
